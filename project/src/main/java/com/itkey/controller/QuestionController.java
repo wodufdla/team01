523cc8d87f2a1adc33bd846828d778e6b88aa57c
Hwangsunae88 <sunea1337@gmail.com>
2023-03-13 16:53:58 +0900

    문의하기 jsp 작업

--- project/src/main/java/com/itkey/controller/QuestionController.java
+++ project/src/main/java/com/itkey/controller/QuestionController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itkey.service.QuestionService;
 
@@ -14,4 +16,15 @@ public class QuestionController {
 	@Autowired
 	private QuestionService questionService;
 
+	//문의하기 리스트 
+	@RequestMapping("/question")
+	public String ask(Model model) {
+		return "question";
+	}
+	
+	//문의하기 글쓰기
+	@RequestMapping("/doAsk" )
+	public String doAsk(Model model) {
+		return "doAsk";
+	}
 }
