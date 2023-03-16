1c9ed40d7cc19a78b667fe75fa07dee97edcfc3b
Hwangsunae88 <sunea1337@gmail.com>
2023-03-16 14:20:16 +0900

    HSA   롬북 설치 후 수정 본 푸쉬 합니다.

--- project/src/main/java/com/itkey/vo/UserVO.java
+++ project/src/main/java/com/itkey/vo/UserVO.java
@@ -2,19 +2,9 @@ package com.itkey.vo;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
-@NoArgsConstructor
 public class UserVO {
 
 	// 회원관리
