743ec42fb5eb027fb1fd099ec9cd7c303340a983
haeri <evergreen0208@hanmail.net>
2023-03-27 15:17:05 +0900

    실시간 공개현황 수정

--- project/src/main/java/com/itkey/controller/ServiceController.java
+++ project/src/main/java/com/itkey/controller/ServiceController.java
@@ -1,11 +1,29 @@
 package com.itkey.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Element;
 
 import com.itkey.service.ServiceService;
 
@@ -16,13 +34,60 @@ public class ServiceController {
 	@Autowired
 	private ServiceService serviceService;
 	
+	public static String getTagValue(String tag, Element element) {
+		NodeList list = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node nValue = (Node) list.item(0);
+		return nValue.getNodeValue();
+	}
+	  
 	@RequestMapping("/safetyMap")
 	public String safetyMap(Model model) {
 		return "safetyMap";
 	}
 	
 	@RequestMapping("/rtPublicStatus")
-	public String rtPublicStatus(Model model) {
+	public String rtPublicStatus(Model model) throws SAXException, IOException, ParserConfigurationException {
+		
+		String url = "http://api.sexoffender.go.kr/openapi/SOCitysStats/";
+		
+		DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+		Document doc = dBuilder.parse(url);
+
+		// root tag 
+		doc.getDocumentElement().normalize();
+		System.out.println("Root element: " + doc.getDocumentElement().getNodeName()); // Root element: result
+		
+		// 파싱할 tag
+		NodeList nList = doc.getElementsByTagName("City");
+		System.out.println("파싱할 리스트 수 : "+ nList.getLength());  // 파싱할 리스트 수 : 18개
+		
+		List<Map<String,Object>> list = new ArrayList<Map<String, Object>>();
+		
+		for(int temp = 0; temp < nList.getLength(); temp++){		
+			Node nNode = nList.item(temp);
+			if(nNode.getNodeType() == Node.ELEMENT_NODE){
+								
+				Element eElement = (Element) nNode;
+				//System.out.println("######################");
+				//System.out.println(eElement.getTextContent());
+
+				String name = getTagValue("city-name", eElement);
+				String count = getTagValue("city-count", eElement);
+				
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("name", name);
+				map.put("count", count);
+				
+				//System.out.println("map" + map);
+				list.add(map);
+			}	// for end
+		}	// if end
+		
+		//System.out.println("list" + list);
+		
+		model.addAttribute("list", list);
+		
 		return "rtPublicStatus";
 	}
 	
--- project/src/main/webapp/WEB-INF/views/rtPublicStatus.jsp
+++ project/src/main/webapp/WEB-INF/views/rtPublicStatus.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,10 +9,46 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+KR:100,200,300,400,500,600,700&amp;subset=korean&amp;display=swap">
 <link rel="stylesheet" href="/resources/css/styles.css">
 <title>실시간 공개현황</title>
+<style type="text/css">
+span {
+	color: #3f78c5;
+}
+</style>
 </head>
 <body>
 <jsp:include page="common/header.jsp" />
+<h3>공개현황 (실시간)</h3>
+<div>
+<h4>> 특별시＊광역시/도 성범죄자 통계</h4>
+<p>
+우리나라의 특별시, 광역시, 각 시도별 성범죄자 통계를 보실 수 있습니다.<br>
+<span>통계는 실제 거주지 주소 공개자 수입니다.</span>
+</p>
+</div>
 
+<div>
+	<table summary="특별시＊광역시/도,성범죄자 수 순서로 성범죄자 통계를를 나타낸 표 입니다">
+		<colgroup>
+			<col width="30%">
+			<col width="70%">
+		</colgroup>
+		<thead>
+		<tr>
+			<th scope="col">특별시＊광역시/도</th>
+			<th scope="col">성범죄자 수</th>
+		</tr>
+		</thead>
+		<tbody>
+			<!-- 지역별 성범죄자 수 -->
+			<c:forEach items="${list }" var="list">
+			<tr>
+				<td>${list.name }</td>
+				<td>${list.count }</td>
+			</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</div>
 <jsp:include page="common/footer.jsp" />
 </body>
 </html>
