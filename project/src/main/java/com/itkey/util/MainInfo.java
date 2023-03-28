8fb665d0b6ff53ffb3a1551d6a8a9812b62c39ba
haeri <evergreen0208@hanmail.net>
2023-03-28 15:42:10 +0900

    오늘 날짜 추출

---
+++ project/src/main/java/com/itkey/util/MainInfo.java
@@ -0,0 +1,17 @@
+package com.itkey.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MainInfo {
+	
+	public String curDate() {
+
+		// 현재 날짜 구하기 (시스템 시계, 시스템 타임존)
+		LocalDate now = LocalDate.now();
+		String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		return date;
+	}
+
+}
