173419daa90e3b38405edfb648566936e882298e
haeri <evergreen0208@hanmail.net>
2023-03-28 20:17:30 +0900

    관리자-매출관리 재업로드

--- project/src/main/java/com/itkey/controller/OrderController.java
+++ project/src/main/java/com/itkey/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.itkey.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,8 +19,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.itkey.pageutil.PageCriteria;
+import com.itkey.pageutil.PageMaker;
 import com.itkey.service.OrderService;
 import com.itkey.service.PaymentService;
+import com.itkey.util.MainInfo;
+import com.itkey.vo.OrderVO;
 import com.itkey.vo.PaymentVO;
 
 
@@ -33,10 +39,6 @@ public class OrderController {
 	private OrderService orderService;
 	@Autowired
 	private PaymentService paymentService;
-	
-	
-
-	
 	
 	@RequestMapping(value="/paymentOk", method=RequestMethod.POST)
 	@ResponseBody
@@ -86,5 +88,54 @@ public class OrderController {
 		mv.setViewName("payRegisterOk");
 		
 		return mv;
+	}
+	
+	@GetMapping("/product")
+	public void productGET(Model model, HttpSession session) {
+		// 매출관리
+		log.info("productGET() 호출");
+		MainInfo mInfo = new MainInfo();
+		String today = mInfo.curDate();
+		String thisMonth = mInfo.curMonth();
+		
+		PageCriteria criteria = new PageCriteria();
+		/*
+		if (keyword != null) {
+			criteria.setKeyword(keyword);
+			criteria.setCategory(category);
+		}
+		if(page != null) {
+			criteria.setPage(page);
+		}
+		
+		if(numsPerPage != null) {
+			criteria.setNumsPerPage(numsPerPage);
+		}
+		*/
+		
+		List<OrderVO> list = orderService.read_list(criteria);
+		model.addAttribute("list", list);
+		
+		// 서비스 유지중
+		int statusY = orderService.select_y();
+		model.addAttribute("statusY", statusY);
+		
+		// 누적 매출액
+		int total = orderService.totalSales();
+		model.addAttribute("totalSales", total);
+		
+		// 월 매출액
+		int totalMonths = orderService.totalMonths(thisMonth);
+		model.addAttribute("totalMonths", totalMonths);
+		
+		// 오늘자 매출액
+		int totalToday = orderService.todaySSales(today);
+		model.addAttribute("totalToday", totalToday);
+		
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCriteria(criteria);
+		pageMaker.setTotalCount(orderService.totalCounts(criteria));
+		pageMaker.setPageData();
+		model.addAttribute("pageMaker", pageMaker);
 	}
 }
--- project/src/main/java/com/itkey/dao/OrderDAO.java
+++ project/src/main/java/com/itkey/dao/OrderDAO.java
@@ -9,4 +9,16 @@ public interface OrderDAO {
 
 	List<OrderVO> select_list(PageCriteria criteria);
 	int totalCounts(PageCriteria criteria);
+	
+	// 서비스 유지중
+	int select_y();
+	
+	// 누적 매출액
+	int totalSales();
+	
+	// 월 매출액
+	int totalMonths(String thisMonth);
+	
+	// 오늘자 매출액
+	int todaySSales(String today);
 }
