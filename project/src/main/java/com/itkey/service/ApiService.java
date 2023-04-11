bc1c922f5951b33e59a462caca932a182285c69c
Hwangsunae88 <sunea1337@gmail.com>
2023-04-11 19:26:53 +0900

    //공공데이터_주차장 운영정보 api, //공공데이터_주차장 실시간 정보 api  서비스단 추가 , 컨트롤러 단 작업 중

--- project/src/main/java/com/itkey/service/ApiService.java
+++ project/src/main/java/com/itkey/service/ApiService.java
@@ -10,6 +10,10 @@ public interface ApiService {
 
 	//공공데이터_주차장 시설정보 api
 	public String PrkSttusInfo() throws Exception;
+	//공공데이터_주차장 운영정보 api
+	public String PrkOprInfo() throws Exception;
+	//공공데이터_주차장 실시간 정보 api
+	public String PrkRealtimeInfo() throws Exception;
 	
 	// 불법 주정차 단속 정보
 	public List<Map<String,Object>> getIllegalParking() throws IOException, ParseException;
